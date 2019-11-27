40ac14ebe372751c4a1465cfef999ee6bbb3e46b
ANikitsenka <52278468+ANikitsenka@users.noreply.github.com>
2019-11-28 00:05:47 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -3,6 +3,18 @@ package com.test;
 import org.apache.commons.collections4.Predicate;
 import org.apache.log4j.Logger;
 
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;import static junit.framework.TestCase.assertTrue;@KarateOptions(tags = {"~@ignore"})
+
+
 class Project {
 
  public String getGreeting() {
@@ -18,16 +30,7 @@ class Project {
   }
 }
 
-import com.intuit.karate.KarateOptions;
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;import static junit.framework.TestCase.assertTrue;@KarateOptions(tags = {"~@ignore"}) 
+ 
 public class TestParallel {
     
 @Test
