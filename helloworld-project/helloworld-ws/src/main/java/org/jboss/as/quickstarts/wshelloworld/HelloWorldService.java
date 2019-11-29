3bc3ae71e24afeda23d5309e2897cc99de9a84a9
stsiapan <55788184+stsiapan@users.noreply.github.com>
2019-11-29 18:44:31 +0300

    added a=b b=a

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldService.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldService.java
@@ -20,13 +20,8 @@ import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-println hello
-println hello
-println hello
-println hello
-println hello
-println hello
-
+def a=b
+def b=a
 /**
  * A simple example of how to setup a JAX-WS Web Service. It can say hello to everyone or to someone in particular.
  *
