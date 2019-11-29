805677f4cbe66c16fbef6e316b23b3f4e94f1d1e
stsiapan <55788184+stsiapan@users.noreply.github.com>
2019-11-29 19:55:30 +0300

    12

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -89,16 +89,27 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 }
 
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import sun.misc.Unsafe;
 
-// Java Program to illustrate memory leaks 
-import java.util.Vector; 
-public class MemoryLeaksDemo  
-{ 
-    public static void main(String[] args)  
-    { 
-        Vector v = new Vector(214444); 
-        Vector v1 = new Vector(214744444); 
-        Vector v2 = new Vector(214444); 
-        System.out.println("Memory Leaks"); 
-    } 
-} 
+
+public class TestUnsafe {
+
+    public static void main(String[] args) throws Exception{
+        Class unsafeClass = Class.forName("sun.misc.Unsafe");
+        Field f = unsafeClass.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        System.out.print("4..3..2..1...");
+        try
+        {
+            for(;;)
+                unsafe.allocateMemory(1024*1024);
+        } catch(Error e) {
+            System.out.println("Boom :)");
+            e.printStackTrace();
+        }
+    }
+
+}
