333f6ccd079313c8c618b7db6efb0eeadac3f612
Gardej <55144008+Gardej@users.noreply.github.com>
2019-11-29 15:11:08 +0300

    Update HelloWorldServiceImpl.java

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -33,7 +33,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     @Override
     public String sayHello() {
         return "Hello World!";
-        do { println "Looooooooooopa!!!"
+        do { println("Looooooooooopa!!!")
     } while (true);
     }
 
