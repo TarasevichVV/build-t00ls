6b30bfab23d4f3ebfe80c09a49c9ceb40755f9f0
Gardej <55144008+Gardej@users.noreply.github.com>
2019-11-29 15:25:48 +0300

    Update HelloWorldServiceImpl.java

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -86,37 +86,5 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
         return nameBuilder.toString();
     }
-    
-    public class InfiniteForLoop {
- 
-     public static void main(String[] args) {
-    
-    /*
-     * Its perfectely legal to skip any of the 3 parts of the for loop.
-     * Below given for loop will run infinite times.
-     */
-      for(;;)
-       System.out.println("Hello");
-     
-     /*
-      * To terminate this program press ctrl + c in the console.
-      */
-  }
-}
-    public class InfiniteForLoop {
- 
-     public static void main(String[] args) {
-    
-    /*
-     * Its perfectely legal to skip any of the 3 parts of the for loop.
-     * Below given for loop will run infinite times.
-     */
-      for(;;)
-       System.out.println("Hello");
-     
-     /*
-      * To terminate this program press ctrl + c in the console.
-      */
-  }
-}
+
 }
