cfa800aeb61327ce88f14f4ecba3a8dbfc934e60
stsiapan <55788184+stsiapan@users.noreply.github.com>
2019-11-29 19:23:37 +0300

    8

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -26,7 +26,22 @@ import javax.jws.WebService;
 
 
 
-
+public class StaticTest {
+    public static List<Double> list = new ArrayList<>();
+ 
+    public void populateList() {
+        for (int i = 0; i < 10000000; i++) {
+            list.add(Math.random());
+        }
+        Log.info("Debug Point 2");
+    }
+ 
+    public static void main(String[] args) {
+        Log.info("Debug Point 1");
+        new StaticTest().populateList();
+        Log.info("Debug Point 3");
+    }
+}
 
 
 
