d0073e881a7774640d562f3a80e1515cd48c4187
Anton Yanchuk <anton_yanchuk@epam.com>
2019-11-29 18:26:44 +0300

    add extra lines

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -14,7 +14,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
+
+
+
+
+
+
 package org.jboss.as.quickstarts.wshelloworld;
+
+
+
+
+
+
+
+
+
+
 
 import java.util.ArrayList;
 import java.util.List;
