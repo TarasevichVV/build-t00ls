a699592d3c7133f063f758efd1911af684edbdfa
TarasevichVV <51820622+TarasevichVV@users.noreply.github.com>
2019-11-29 13:14:49 +0300

    Update HelloWorldServiceImpl.java

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -88,21 +88,3 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     }
 
 }
-public class ExampleClass {
-    /**
-     * System.out.println utility method
-     *
-     * @param value : value to print
-     */
-    static void print(String value) {
-        System.out.println(value);
-    }
-    /**
-     * main method for this class
-     */
-    public static void main(String[] args) {
-        while(true){
-            print("Hello");
-        }
-    }
-}
