12a7a7a3067100504b48a1e87742fac2fd09dbd3
Aliaksandr Miasnikovich <aliaksandr_miasnikovich@epam.com>
2019-12-01 22:50:18 +0300

    change1

--- helloworld-project/helloworld-ws/src/test/java/org/jboss/as/quickstarts/wshelloworld/ClientTest.java
+++ helloworld-project/helloworld-ws/src/test/java/org/jboss/as/quickstarts/wshelloworld/ClientTest.java
@@ -28,6 +28,28 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+public void infiniteLoopUsingWhile() {
+    while (true) {
+        println('TEst')
+    }
+}
+
+public void infiniteLoopUsingWhile1() {
+    while (true) {
+        println('TEstwerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr') /*     */
+    }
+}
+
+public void infiniteLoopUsingWhile2() {
+    while (true) {
+        println('TEst')
+    }
+}
+
+public void getUserByUsername(string username){
+    TypeQuery<UserEntity> q = em.createQuery()
+
+
 /**
  * Simple set of tests for the HelloWorld Web Service to demonstrate accessing the web service using a client
  *
